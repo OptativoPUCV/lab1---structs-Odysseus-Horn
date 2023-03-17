5f44b741e2c29e8cb22299ab652770d13ac4b775
Replit user <>
2023-03-17 15:04:44 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -94,25 +94,48 @@ Ejercicio 5a.
 Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
-void asignarValor(Vector *v, int i, int valor) { v->datos[i] = valor; }
+void asignarValor(Vector *v, int i, int valor) 
+
+{ v->datos[i] = valor; 
+  
+  
+  
+}
 
 /*
 Ejercicio 6.
 Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
-int obtenerValor(Vector *v, int i) { return 0; }
+int obtenerValor(Vector *v, int i) 
+{ 
+  
+  return v->datos[i]; 
+  
+}
 
 /*
 Ejercicio 7.
 Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
-void sumaV(Vector *a, Vector *b, Vector *c) {}
+void sumaV(Vector *a, Vector *b, Vector *c) 
+{  
+  for(int i = 0 ; i < c->capacidad ; i++)
+    {
+     c->datos[i] = a->datos[i] + b->datos[i];
+    }
+}
 
 /*
 Ejercicio 8.
 Use las operaciones implementadas de vectores para
-sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
+sumar (a1,a2)+(b1,b2). Almacene el resultado en el vector c.
 */
-void sumaV2(int a1, int a2, int b1, int b2, Vector *c) {}
+void sumaV2(int a1, int a2, int b1, int b2, Vector *c) 
+{
+  
+
+
+  
+}
